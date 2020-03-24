d9f3638ef1d833544d5a8a9646eb769ebe63b3ca
Jarne Deschacht <jarne.deschacht@student.hogent.be>
2020-03-24 19:31:13 +0100

    added example of how to split dataset to apply cross validation using sklearn

---
+++ scripts/applyCrossValidation.py
@@ -0,0 +1,13 @@
+# code based on https://www.youtube.com/watch?v=gJo0uNL-5Qw
+# this code will apply 5 fold cross validation (CV) for the dataset
+
+# import the library to apply (CV)
+from sklearn.model_selection import KFold
+
+kf = KFold(n_splits=5, shuffle=True)
+
+for i in range(15):
+    print(i)
+    for train_index, test_index in kf.split([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, ]):
+        print(train_index, test_index)
+    print('_______________________________________________________')
